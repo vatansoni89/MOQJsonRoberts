7ebb8a43c6525d1496146d14f20222d9dc2295d8
Vatan Soni <vatansoni89@gmail.com>
2020-01-12 09:10:43 +0530

    Mocking:    FunctionWithReturnType:

--- Loans.Tests/LoanApplicationProcessorShould.cs
+++ Loans.Tests/LoanApplicationProcessorShould.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Loans.Domain.Applications;
+using Moq;
 using NUnit.Framework;
 
 namespace Loans.Tests
@@ -17,11 +19,34 @@ namespace Loans.Tests
             var loanAmount = new LoanAmount("inr",3500000);
             var loanApplication = new LoanApplication(1, loanProduct,loanAmount, "Vatan",35,"Ekta Nagar",100000 );
 
-            var lap = new LoanApplicationProcessor(null, null);
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mockCreditScorer = new Mock<ICreditScorer>();
+            var lap = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
             lap.Process(loanApplication);
 
             //It fails as LoanApplicationProcessor dont accept Null arguments. 
             Assert.That(loanApplication.GetIsAccepted(),Is.False);
+        }
+
+        [Test]
+        public void Accept()
+        {
+            
+            var loanProduct = new LoanProduct(1,"Loan",8.35m);
+            var loanAmount = new LoanAmount("inr",3500000);
+            var loanApplication = new LoanApplication(1, loanProduct,loanAmount, "Vatan",35,"Ekta Nagar",100000 );
+
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            mockIdentityVerifier.Setup(x => x.Validate("Vatan", 35, "Ekta Nagar")).Returns(true);
+
+            var mockCreditScorer = new Mock<ICreditScorer>();
+            //mockCreditScorer.Setup(x => x.CalculateScore("Vatan", "Ekta Nagar")); //It returns null so could not be used.
+            
+            var lap = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
+            lap.Process(loanApplication);
+
+            //It fails as LoanApplicationProcessor dont accept Null arguments. 
+            Assert.That(loanApplication.GetIsAccepted(),Is.True);
         }
     }
 }
